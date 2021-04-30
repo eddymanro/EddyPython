c9faf67d779e467937cc2279a0fb1eb3444c5919
Eddy Cos <EddyMacbook@Eddys-MacBook-Pro.local>
2021-04-30 18:13:10 +0300

    new modifications and folder sturcture

--- Lists/main.py
+++ Lists/main.py
@@ -1,8 +1,10 @@
 # lists
 
 def __main__():
-    my_list = [1, 2, 3, 4, 10, 6, 7, 23]
-    print(my_list[4])
+    my_numbers_list = [1, 2, 3, 4, 10, 6, 7, 23]
+    my_string_list = ['Oana', 'Ioana', 'Joe']
+    print(my_numbers_list[4])
+    print(my_string_list[1])
 
 
 __main__()
